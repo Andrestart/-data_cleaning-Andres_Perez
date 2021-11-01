279fde25aa6fd14c83ec0f598451c8c49fc211d3
Andrestart <andres.perez.tercero@gmail.com>
2021-11-01 17:43:36 +0100

    trabajito terminado

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# -data_cleaning-Andres_Perez
+This is Andres Perez's shark attack data cleaning and visualization project.
+
+![portada](https://github.com/Andrestart/data_cleaning_andres_perez/portada.jpg)
+
+This is the explanation of the project.
+If you open the file "1_data_cleaning.ipynb", you can start visualizing the project. You will need to have jupyter notebook installed, as well as the libraries specified in the first cell.
+
+After importing these libraries, you can go on executing the cells. You will be directed through the jupyter notebook markdown and code cells, that are explanatory of the data cleaning process. In order to fully understand this process, you will need to open the "cleaning_functions.py" file, where I have created my own functions that have been imported as new library called "src.cleaning_functions" as "cf".
+
+All functions in "src.cleaning_functions" are commented and explained, please read them in order to understand their functioning.
+
+During my stay in Australia, I always heard that surfers are the most attacked by sharks, but nobody had the proof. This is my opportunity to know if that's correct or just a myth. It was also heard that summer was the most dangerous seasons when it comes to shark attacks.
+
+Because of what I explained above, I have chosen 2 hypothesis that I want to prove after cleaning this data, which are:
+Do sharks attack more often in Australia on summer?
+Are surfers the most targeted by sharks?
+
+I have deleted the rows and columns that are not needed in order to prove these hypothesis.
+
+After having read the file "1_data_cleaning.ipynb", open "2_graphs.ipynb" to see the graphs and their conclusion.
+
+You can also find the cleaned tables and these graphs in the folder "graphs_and_tables".
--- src/cleaning_functions.py
+++ src/cleaning_functions.py
@@ -1,30 +1,40 @@
 import re
 from typing import Collection
+import numpy as np
 
 def prepare(col):
     """
-    Esta función convierte datos para facilitar su análisis.
+    This function converts a panda series into a new one with the data converted into more "programming-friendly" strings in order to ease data analysis.
     Args:
-    col(pandas series): serie de datos que queremos convertir.
+    col(pandas series): panda data series that we want to convert.
     return:
-    Convierte la serie en lista y devuelve los elementos en minúscula, sin espacios a los lados y reemplaza espacio (" ") por _ ("_")
+    list of elements converted into lower case, no spaces no the sides and replaces the spaces in between the words with a low bar(_).
     """  
-    col = list(col)
-    return [i.lower().strip().replace(" ","_") for i in col]
+    return [i.lower().strip().replace(" ","_") for i in list(col)]
 
-def uniform(data,findthis,replaceforthis):
-    new = []
-    for i  in list(data):
-        if findthis in i:
-            new.append(re.sub((f'^\w.+({findthis})*.'),str(replaceforthis),i))
-        else:
-            new.append(i)
-    return new
+
 
 def strdata(col):
+    """
+    This function casts data to string in order to ease its analysis.
+    Args:
+    col(pandas series): panda data series that we want to convert.
+    return:
+    list of elements casted into string.
+    """
     return [str(i) for i in col]
-    
-def uniforme(data,findthis,replaceforthis):
+
+
+def unify(data,findthis,replaceforthis):
+    """
+    It finds a chosen word contained in a string and replaces it for another one, making it easier to unify categories.
+    Args:
+        Arg1(data):pandas data series (or column in a dataframe) previously casted into string.
+        Arg2(findthis): chosen word that we want to look for in Arg1. All that contains this word will be taken into account.
+        Arg3(replaceforthis): chosen word that we want to get instead of Arg2 word.
+    return:
+    list of words replaced with the word Arg3.
+    """
     new = []
     for i  in list(data):
         if findthis in i:
@@ -32,3 +42,149 @@ def uniforme(data,findthis,replaceforthis):
         else:
             new.append(i)
     return new
+
+def fatal(col):
+    """
+    It unifies the values in a column where you only find "y" or "n" and useless values, creating a NaN in the ones that are not "y" or "n".
+    Args:
+    col(pandas series): panda data series (or column in a dataframe) previously casted into string.
+    return:
+    list of values as "y", "n" or NaN.
+    """
+    for i in col:
+        if i == "y":
+            return "yes"
+        elif i == "n":
+            return "no"
+        else:
+            return np.nan
+
+def act(col):
+    """
+    This function replaces the activities that have a minor importance for the string "other_activities".
+    Args:
+    col(pandas series): panda data series (or column in a dataframe) previously casted into string.
+    return:
+    list of converted data that is not part of a previously set relevant category into the string "other_activities".
+    """
+    for activity in col:
+        if col == "swimming":
+            return "swimming"
+        elif col == "surfing":
+            return "surfing"
+        elif col == "fishing":
+            return "fishing"
+        elif col == "boat":
+            return "boat"
+        elif col == None:
+            pass
+        else:
+            return "other_activities"
+
+def getmonth(col):
+    """
+    It takes into account the positions where the month is expressed and converts them into the "whole" month word.
+    Args:
+    col(pandas series): panda data series (or column in a dataframe) previously casted into string.
+    return:
+    list of months expressed in a complete word. If no "month word" is found, it returns NaN.
+    """
+    for month in col:
+        if col[3:6] == "jan" or "jan" in col:
+            return "january"
+        elif col[3:6] == "feb" or "feb" in col:
+            return "february"
+        elif col[3:6] == "mar" or "mar" in col:
+            return "march"
+        elif col[3:6] == "apr" or "apr" in col:
+            return "april"
+        elif col[3:6] == "may" or "may" in col:
+            return "may"
+        elif col[3:6] == "jun" or "jun" in col:
+            return "june"
+        elif col[3:6] == "jul" or "jul" in col:
+            return "july"
+        elif col[3:6] == "aug" or "aug" in col:
+            return "august"
+        elif col[3:6] == "sep" or "sep" in col:
+            return "september"
+        elif col[3:6] == "oct" or "oct" in col:
+            return "october"
+        elif col[3:6] == "nov" or "nov" in col:
+            return "november"
+        elif col[3:6] == "dec" or "dec" in col:
+            return "december"
+        else:
+            return np.nan
+
+def australia(col):
+    """
+    This function takes a column and converts the strings not equal to australia into Nan.
+    Args:
+    col(pandas series): panda data series (or column in a dataframe) previously casted into string.
+    return:
+    list of data with strings equal to "australia" or otherwise NaN.
+    """
+    for country in col:
+        if col == "australia":
+            return "australia"
+        else:
+            return np.nan
+
+def season(col):
+    """
+    It takes the months words and converts them into the corresponding season name in the southern hemisphere. If no month words are found, it creates NaNs.
+    Args:
+    col(pandas series): panda data series (or column in a dataframe) previously casted into string.
+    return:
+    name of the corresponding season in the souther hemisphere. If no month words are found, it creates NaNs.
+    """
+    for i in str(col):
+        if col == "december":
+            return "summer"
+        elif col == "january":
+            return "summer"
+        elif col == "february":
+            return "summer"
+        elif col == "march":
+            return "autumn"
+        elif col == "april":
+            return "autumn"
+        elif col == "may":
+            return "autumn"
+        elif col == "june":
+            return "winter"
+        elif col == "july":
+            return "winter"
+        elif col == "august":
+            return "winter"
+        elif col == "september":
+            return "spring"
+        elif col == "october":
+            return "spring"
+        elif col == "november":
+            return "spring"
+        else:
+            return np.nan
+
+def actnan(col):
+    """
+    This function replaces the activities that have a minor importance for NaNs.
+    Args:
+    col(pandas series): panda data series (or column in a dataframe) previously casted into string.
+    return:
+    list of converted data that is not part of a previously set relevant category into NaNs.
+    """
+    for activity in col:
+        if col == "swimming":
+            return "swimming"
+        elif col == "surfing":
+            return "surfing"
+        elif col == "fishing":
+            return "fishing"
+        elif col == "boat":
+            return "boat"
+        elif col == np.nan:
+            pass
+        else:
+            return np.nan
